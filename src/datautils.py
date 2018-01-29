7c26ddeb2b1343398175b3bebe89608ec9b7e1e2
Unknown <lorenz.ohly@gmail.com>
2018-01-29 21:03:07 +0100

    Added tyoe annotations

--- src/datautils.py
+++ src/datautils.py
@@ -1,7 +1,17 @@
+# !/bin/python
+"""
+Utilities for files
+"""
 import os
+from typing import List, Union
 
 
-def get_filenames(folder, fullpath=True, sort=True, extensions=None):
+def get_filenames(
+        folder: str,
+        fullpath: bool = True,
+        sort: bool = True,
+        extensions: Union[None, List[str]] = None) -> List[str]:
+    """Gets all files in folder and filters/transforms them"""
     filenames = os.listdir(folder)
     if extensions:
         filenames = [filename for filename in filenames if has_extensions(filename, extensions)]
@@ -11,22 +21,22 @@ def get_filenames(folder, fullpath=True, sort=True, extensions=None):
         filepaths = [os.path.join(folder, filename)
                      for filename in filenames]
         return filepaths
-    else:
-        return filenames
+    return filenames
 
 
-def has_extensions(filename, extensions):
+def has_extensions(filename: str, extensions: List[str]) -> bool:
     """Checks if a file has one of several extensions"""
     return filename.split(sep='.')[-1] in extensions
 
 
-def sort_filenames(filenames):
+def sort_filenames(filenames: List[str]) -> List[str]:
     """Sorts a list of file names based on a number in the file name"""
     return sorted(filenames, key=lambda x: int(''.join([c for c in x if c in '0123456789'])))
 
-def parse_dat(dat_path):
-    with open(dat_path) as f:
-        lines = f.readlines()
+def parse_dat(dat_path: str) -> List[int]:
+    """Parses ground truth .dat file from original dataset"""
+    with open(dat_path) as file:
+        lines = file.readlines()
     digits = [int(x) for x in ''.join(lines[2:]) if x in '0123456789']
     assert len(digits) == 81
     return digits
